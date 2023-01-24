8ac498dc03a53bcc65c346c844f641bd0443d0bb
lipkij <lipkij@gmail.com>
2023-01-24 14:00:56 +0400

    refactor: split startDancer to functions

--- scripts/tableDancer.js
+++ scripts/tableDancer.js
@@ -1,29 +1,33 @@
+window.addEventListener('load', startDancer);
+
+function startDancer(event) {
+    let tables = document.getElementsByTagName('table');
+    for (const tableElement of tables) {
+        markTables(tableElement);
+        addTableListeners(tableElement);
+    }
+}
+
+function addTableListeners(tableElement) {
+    tableElement.addEventListener('mouseover', fillSelection);
+    tableElement.addEventListener('mouseout', emptySelection);
+}
+
+function markTables(tableElement){
+    tableElement.style.border = "4px dashed red";
+}
 
 function fillSelection(event) {
-    if (event.target.nodeName == 'TABLE'){
+    if (event.target.nodeName == 'TABLE') {
         event.target.style.border = "8px solid blue";
     }
 }
 
 function emptySelection(event) {
-    if (event.target.nodeName == 'TABLE'){
+    if (event.target.nodeName == 'TABLE') {
         event.target.style.border = "4px dashed red";
     }
 }
-
-
-
-function startDancer(event){
-    let tables = document.getElementsByTagName('table');
-    for (const tableElement of tables){
-        tableElement.style.border = "4px dashed red";
-        tableElement.addEventListener('mouseover', fillSelection);
-        tableElement.addEventListener('mouseout', emptySelection);
-    }
-    
-}
-
-window.addEventListener('load', startDancer);
 
 
 
