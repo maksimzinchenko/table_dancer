39e308274648cf5f998fce1e96f61b7a3f82394a
lipkij <lipkij@gmail.com>
2023-01-24 22:27:10 +0400

    add: row and column selection. fist tests

--- scripts/tableDancer.js
+++ scripts/tableDancer.js
@@ -2,7 +2,14 @@ const tableDancerParams = {
     'selectTable': true
 }
 
+let dragDropElement;
+let dragX, dragY;
+let dragRow, dragColumn;
+let startDrag = false;
+
 window.addEventListener('load', initDancer);
+document.body.addEventListener('mousedown', startDragListener);
+document.body.addEventListener('mouseup', dropDragListener);
 
 async function addMessageListeners() {
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -41,6 +48,39 @@ async function readOldDancerParamsFromStorage() {
 }
 
 
+function startDragListener(event){
+    if (['TD'].includes(event.target.nodeName)){
+        dragDropElement = event.target;
+        dragX = event.clientX;
+        dragY = event.clientY;
+        startDrag = true;
+        event.preventDefault();
+    } 
+} 
+
+function dropDragListener(event){
+    if (startDrag){
+        if (Math.abs(dragX - event.clientX) > Math.abs(dragY - event.clientY)){
+            parentRow = dragDropElement.parentNode;
+            parentRow.classList.add('table_row_select');
+        } else {
+            tableRowsParent = dragDropElement.parentNode.parentNode;
+            tdIndex = dragDropElement.cellIndex;
+
+            for (tableRow of tableRowsParent.children){
+                console.log(tableRow.children[tdIndex]);
+                if (!!tableRow.children[tdIndex]){
+                    tableRow.children[tdIndex].classList.add('table_row_select');
+                }
+                
+            }
+        }
+        startDrag = false;
+        event.preventDefault();
+    }
+}
+
+
 function markupBorderTables() {
     let tables = document.getElementsByTagName('table');
     if (tableDancerParams['selectTable'] === true) {
@@ -66,18 +106,23 @@ function addAllTableBorderHoverListener() {
 function addTableListeners(tableElement) {
     tableElement.addEventListener('mouseover', fillSelection);
     tableElement.addEventListener('mouseout', emptySelection);
+    tableElement.addEventListener('click', event => {
+        if (['TABLE', 'TR', 'TD'].includes(event.target.nodeName)) {
+            event.target.classList.toggle('table_select');
+        }
+    })
 }
 
 
 
 function fillSelection(event) {
-    if (event.target.nodeName == 'TABLE') {
+    if (['TABLE', 'TR', 'TD'].includes(event.target.nodeName)) {
         event.target.classList.add('hover_select');
     }
 }
 
 function emptySelection(event) {
-    if (event.target.nodeName === 'TABLE') {
+    if (['TABLE', 'TR', 'TD'].includes(event.target.nodeName)) {
         event.target.classList.remove('hover_select');
     }
 }
